09bf1c9df0dcf57bf80438cac3a7388546126cdc
kishida <naokikishida@gmail.com>
2013-12-11 12:51:57 +0900

    Webアプリ：画面フロー作製

---
+++ TesteeWebApp/src/main/java/kis/testee/flow/PurchaseController.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kis.testee.flow;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.flow.FlowScoped;
+import javax.inject.Named;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author naoki
+ */
+@Named
+@FlowScoped(PurchaseFlowDef.flowId)
+public class PurchaseController implements Serializable{
+    @Setter @Getter
+    boolean member;
+    
+    @Setter @Getter
+    @NotNull
+    String address;
+    
+    @Setter @Getter
+    @NotNull
+    String shipName;
+    
+    @Setter @Getter
+    @NotNull @Size(min = 1, max=8)
+    String userName;
+    
+    @Setter @Getter 
+    @NotNull @Size(min = 1, max=8)
+    String password;
+    
+    @Setter @Getter
+    @NotNull
+    @Min(1) @Max(10)
+    Integer amount;
+    
+    @Setter @Getter
+    Product product;
+    
+    @AllArgsConstructor
+    @Setter @Getter
+    @EqualsAndHashCode(of = "name")
+    public class Product{
+        String name;
+        int price;
+    }
+    
+    public List<Product> getProductList(){
+        return Arrays.asList(
+                new Product("かわのよろい", 50), 
+                new Product("もうふのよろい", 150), 
+                new Product("だきまくらのたて", 70));
+    }
+    
+    public void initialize(){
+        member = false;
+        address = "";
+        shipName = "";
+        userName = "";
+        password = "";
+        amount = null;
+        product = null;
+    }
+    
+    public String guest(){
+        member = false;
+        return "product";
+    }
+    
+    public String login(){
+        if(userName != null && !userName.isEmpty() && userName.equals(password)){
+            member = true;
+            return "product";
+        }else{
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, 
+                    "IDとパスワードが一致しません", "IDとパスワードが一致しません"));
+            return null;
+        }
+    }
+    
+    public void finish(){
+        System.out.println("finish!");
+    }
+}
---
+++ TesteeWebApp/src/main/java/kis/testee/flow/PurchaseFlowDef.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kis.testee.flow;
+
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Produces;
+import javax.faces.flow.Flow;
+import javax.faces.flow.builder.FlowBuilder;
+import javax.faces.flow.builder.FlowBuilderParameter;
+import javax.faces.flow.builder.FlowDefinition;
+import javax.inject.Named;
+
+/**
+ *
+ * @author naoki
+ */
+@Named
+@Dependent
+public class PurchaseFlowDef {
+    
+    public final static String flowId = "purchaseFlow";
+    public String getFlowId() {
+        return flowId;
+    }
+    
+    @Produces
+    @FlowDefinition
+    public Flow defineFlow(@FlowBuilderParameter FlowBuilder builder){
+        builder.initializer("#{purchaseController.initialize()}");
+        builder.id("", flowId);
+        builder.returnNode("index").fromOutcome("/index");
+        builder.viewNode(flowId, "/purchasestart").markAsStartNode();
+        builder.viewNode("login", "/purchaselogin");
+        builder.viewNode("address", "/purchaseaddress");
+        builder.viewNode("product", "/purchaseproduct");
+        builder.viewNode("payment", "/purchasepayment");
+        builder.viewNode("confirm", "/purchaseconfirm");
+        builder.viewNode("complete", "/purchasecomplete");
+        builder.switchNode("gotopay")
+                .defaultOutcome("payment")
+                .switchCase()
+                .condition("#{purchaseController.member}")
+                .fromOutcome("confirm");
+        builder.switchNode("backtopay")
+                .defaultOutcome("payment")
+                .switchCase()
+                .condition("#{purchaseController.member}")
+                .fromOutcome("product");
+        builder.finalizer("#{purchaseController.finish()}");
+        return builder.getFlow();
+    }
+}
